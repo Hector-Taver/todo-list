35bf6e473ee1a251caeb32a60d5fc17f85bc7bec
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-04-10 14:38:54 -0300

    create styles for EmptyTask component

--- src/components/EmptyTask/index.tsx
+++ src/components/EmptyTask/index.tsx
@@ -1,10 +1,19 @@
+import { ClipboardText } from 'phosphor-react'
 import { TasksHeader } from '../TasksHeader'
+
+import styles from './EmptyTask.module.css'
 
 export function EmptyTasks() {
   return (
     <>
       <TasksHeader completed={0} created={0} />
-      <h1>Empty Task</h1>
+      <div className={styles.container}>
+        <ClipboardText size={60} opacity={0.5} />
+        <p>
+          <span>Você ainda não tem tarefas cadastradas</span>
+          Crie tarefas e organize seus itens a fazer
+        </p>
+      </div>
     </>
   )
 }
